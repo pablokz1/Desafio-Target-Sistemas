d4bfe9248a1495f8d3e77ec1c39d09a633bfdcf0
Pablokz1 <pablo.carvalho16@hotmail.com>
2025-01-20 13:28:27 -0300

    fix: corrige questão 3 para receber um json

--- 3.js
+++ 3.js
@@ -1,35 +1,5 @@
-const faturamentoMensal = [
-    { "dia": 1, "valor": 221.5 },
-    { "dia": 2, "valor": 430.0 },
-    { "dia": 3, "valor": 0 },
-    { "dia": 4, "valor": 0 },
-    { "dia": 5, "valor": 190.0 },
-    { "dia": 6, "valor": 0 },
-    { "dia": 7, "valor": 340.0 },
-    { "dia": 8, "valor": 120.5 },
-    { "dia": 9, "valor": 0 },
-    { "dia": 10, "valor": 450.0 },
-    { "dia": 11, "valor": 300.0 },
-    { "dia": 12, "valor": 0 },
-    { "dia": 13, "valor": 0 },
-    { "dia": 14, "valor": 275.0 },
-    { "dia": 15, "valor": 360.0 },
-    { "dia": 16, "valor": 410.0 },
-    { "dia": 17, "valor": 0 },
-    { "dia": 18, "valor": 500.0 },
-    { "dia": 19, "valor": 0 },
-    { "dia": 20, "valor": 0 },
-    { "dia": 21, "valor": 390.0 },
-    { "dia": 22, "valor": 200.0 },
-    { "dia": 23, "valor": 0 },
-    { "dia": 24, "valor": 460.0 },
-    { "dia": 25, "valor": 0 },
-    { "dia": 26, "valor": 320.0 },
-    { "dia": 27, "valor": 0 },
-    { "dia": 28, "valor": 250.0 },
-    { "dia": 29, "valor": 400.0 },
-    { "dia": 30, "valor": 0 }
-];
+const dados = require('./dados.json');
+const faturamentoMensal = dados;
 
 function analisarFaturamento(faturamento) {
     const valoresValidos = faturamento
